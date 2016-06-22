10a620bd57adb6b750ed444fa11b37b831a786c9
Adam Hellberg <sharparam@sharparam.com>
2016-06-22 03:44:24 +0200

    Add support for sending messages in API

--- lib/ratrix/matrix/api.rb
+++ lib/ratrix/matrix/api.rb
@@ -1,11 +1,20 @@
 require 'httparty'
 
+module Ratrix::Matrix
 class Ratrix::Matrix::API
     include HTTParty
 
+    headers({
+        'User-Agent' => "ratrix/#{Ratrix::VERSION}",
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+    })
+
+    debug_output $stdout
+
     DEFAULT_ENDPOINT = 'https://matrix.org'
 
-    DEFAULT_API_PATH = '/_matrix/client/api/v1'
+    DEFAULT_API_PATH = '/_matrix/client/r0'
 
     ##
     # Initializes a new instance of Ratrix::Matrix::API.
@@ -29,21 +38,19 @@ class Ratrix::Matrix::API
         @endpoint = endpoint
         @api_path = api_path
 
-        base_uri @endpoint + @api_path
+        @base_uri = @endpoint + @api_path
 
-        headers { 'User-Agent' => "ratrix/#{Ratrix::VERSION}" }
+        @transaction_id = 0
 
         return unless token
 
-        auth_token = token
-        default_params { auth_token: auth_token }
+        @auth_token = token
     end
 
     ##
     # Sets the authorization token to use for requests.
     def auth_token=(token)
         @auth_token = token
-        default_params { auth_token: @auth_token }
     end
 
     ##
@@ -53,10 +60,40 @@ class Ratrix::Matrix::API
     end
 
     def send_message_event(room, type, content)
-        @@put "/rooms/#{room}/send/#{type}", content
+        room = get_room_id room if room.start_with? '#'
+        @transaction_id += 1
+        make_request :put, "/rooms/#{room}/send/#{type}/#{@transaction_id}", content
     end
 
     def send_message(room, content, type = 'm.text')
-        @send_message_event room, 'm.room.message', { msgtype: type, body: content }
+        send_message_event room, 'm.room.message', { msgtype: type, body: content }
     end
+
+    def get_room_id(room_alias)
+        response = make_request :get, "/directory/room/#{room_alias}"
+
+        case response.code
+        when 200
+            response['room_id']
+        else
+            nil
+        end
+    end
+
+    private
+
+    def make_request(method, path, content = {}, &block)
+        path = @base_uri + URI::encode(path)
+        options = { query: {access_token: @auth_token}, body: content.to_json }
+
+        case method
+        when :get
+            self.class.get path, options, &block
+        when :put
+            self.class.put path, options, &block
+        when :post
+            self.class.post path, options, &block
+        end
+    end
+end
 end
