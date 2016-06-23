4721c98bbfa10e0c5a84dc2a2f1f7e538096b12b
Adam Hellberg <sharparam@sharparam.com>
2016-06-23 04:18:26 +0200

    Change require order in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
+# It only makes sense to use CodeClimate reporter on Travis
+if ENV['TRAVIS']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'chatrix'
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
