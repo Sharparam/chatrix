ff620e707af12aa8ca573b040ddfc364191d3c37
Adam Hellberg <sharparam@sharparam.com>
2016-06-23 13:10:07 +0200

    Move version tests to its own file

--- spec/chatrix_spec.rb
+++ spec/chatrix_spec.rb
@@ -1,11 +1,2 @@
 describe Chatrix do
-  it 'has a version number' do
-    expect(Chatrix::VERSION).not_to be nil
-  end
-
-  it 'has a correctly formatted version number' do
-    # Version has to be X.Y.Z, optionally followed by additional
-    # version metadata
-    expect(Chatrix::VERSION).to match(/^\d+\.\d+\.\d+(\.\w+)*$/)
-  end
 end
