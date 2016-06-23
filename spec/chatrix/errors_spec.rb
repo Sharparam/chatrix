9776adc8c99ce9e6569cfe4855c6e0faf5d79165
Adam Hellberg <sharparam@sharparam.com>
2016-06-23 22:54:28 +0200

    Add tests for RateLimitError

--- spec/chatrix/errors_spec.rb
+++ spec/chatrix/errors_spec.rb
@@ -11,6 +11,48 @@ describe Chatrix::RequestError do
   end
 end
 
+describe Chatrix::RateLimitError do
+  context 'with provided delay' do
+    let(:response) do
+      { 'errcode' => 'M_FOO', 'error' => 'slow down', 'retry_after_ms' => 1234 }
+    end
+
+    let(:error) { Chatrix::RateLimitError.new response }
+
+    it 'should set the correct delay' do
+      expect(error.retry_delay).to eql response['retry_after_ms']
+    end
+
+    it 'should have an error code' do
+      expect(error.code).to eql response['errcode']
+    end
+
+    it 'should have an error message' do
+      expect(error.api_message).to eql response['error']
+    end
+  end
+
+  context 'without a delay' do
+    let(:response) do
+      { 'errcode' => 'M_NO_DELAY', 'error' => 'how long?' }
+    end
+
+    let(:error) { Chatrix::RateLimitError.new response }
+
+    it 'should not set a delay' do
+      expect(error.retry_delay).to be nil
+    end
+
+    it 'should have an error code' do
+      expect(error.code).to eql response['errcode']
+    end
+
+    it 'should have an error message' do
+      expect(error.api_message).to eql response['error']
+    end
+  end
+end
+
 describe Chatrix::UserNotFoundError do
   let(:user) { '@user:host.tld' }
   let(:error) { Chatrix::UserNotFoundError.new user }
