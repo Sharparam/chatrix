cfd190c24777d21594e67e6f654530a2893ea333
Adam Hellberg <sharparam@sharparam.com>
2016-06-23 14:38:19 +0200

    Add some tests for matrix class

---
+++ spec/chatrix/matrix_spec.rb
@@ -0,0 +1,23 @@
+describe Chatrix::Matrix do
+  context 'when created with no args' do
+    it 'has a default homeserver' do
+      expect(subject.homeserver).to be_a String
+    end
+
+    it 'does not set an access token' do
+      expect(subject.access_token).to be nil
+    end
+  end
+
+  context 'when created with args' do
+    let(:api) { Chatrix::Matrix.new 'my_token', 'my_server' }
+
+    it 'sets a custom homeserver' do
+      expect(api.homeserver).to eql 'my_server'
+    end
+
+    it 'sets a custom token' do
+      expect(api.access_token).to eql 'my_token'
+    end
+  end
+end
