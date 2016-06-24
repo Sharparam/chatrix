70d5e5c1416a7dfd2e4e14099f09bf955c2c9e0a
Adam Hellberg <sharparam@sharparam.com>
2016-06-24 03:28:45 +0200

    Add a Users class for managing users

---
+++ lib/chatrix/users.rb
@@ -0,0 +1,37 @@
+require 'chatrix/user'
+
+require 'wisper'
+
+module Chatrix
+  class Users
+    include Wisper::Publisher
+
+    def initialize(matrix)
+      @matrix = matrix
+
+      # user_id => user
+      @users = {}
+    end
+
+    def [](id)
+      return @users[id] if id.start_with? '@'
+
+      res = @users.find { |i, u| u.displayname == id }
+      res.last if res.respond_to? :last
+    end
+
+    def process_member_event(room, event)
+      get_user(event['sender']).process_member_event room, event
+    end
+
+    private
+
+    def get_user(id)
+      return @users[id] if @users.key? id
+      user = User.new id, @matrix
+      @users[id] = user
+      broadcast(:added, user)
+      user
+    end
+  end
+end
