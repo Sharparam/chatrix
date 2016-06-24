57db7a02485271a8950f19649ec93bbdf920a43c
Adam Hellberg <sharparam@sharparam.com>
2016-06-24 19:30:12 +0200

    Add docs for EventProcessor

--- lib/chatrix/event_processor.rb
+++ lib/chatrix/event_processor.rb
@@ -1,20 +1,36 @@
 module Chatrix
   # Keeps track of what events have already been processed.
   class EventProcessor
+    # Initializes a new EventProcessor instance.
     def initialize
       @processed = []
     end
 
+    # Marks an event as having been processed.
+    # @param event [String,Hash] The affected event.
     def processed(event)
       @processed.push parse_event event
     end
 
+    # Checks if an event has been processed.
+    #
+    # @param event [String,Hash] The event to check.
+    # @return [Boolean] `true` if the event has been processed,
+    #   otherwise `false`.
     def processed?(event)
       @processed.member? parse_event event
     end
 
     private
 
+    # Extract the event ID from an event object.
+    # If this is a string, it's returned verbatim.
+    #
+    # @param event [String,Hash] The event object to extract information from.
+    # @return [String] The event ID.
+    #
+    # @raise ArgumentError if the event object is of an invalid type or does
+    #   not contain an ID.
     def parse_event(event)
       if event.is_a? String
         event
