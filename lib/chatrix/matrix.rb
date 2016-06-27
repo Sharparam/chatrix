f674be8db1afe757b874197452f5f341018cdb20
Adam Hellberg <sharparam@sharparam.com>
2016-06-27 08:22:03 +0200

    Make some code neater

--- lib/chatrix/matrix.rb
+++ lib/chatrix/matrix.rb
@@ -93,8 +93,7 @@ module Chatrix
     # @return [Array<String>] an array with the supported versions.
     def versions
       make_request(
-        :get,
-        '/versions',
+        :get, '/versions',
         base: "#{@homeserver}/_matrix/client"
       )['versions']
     end
@@ -160,10 +159,8 @@ module Chatrix
     # @return [Hash] the search results.
     def search(from: nil, options: {})
       make_request(
-        :post,
-        '/search',
-        params: { next_batch: from },
-        content: options
+        :post, '/search',
+        params: { next_batch: from }, content: options
       ).parsed_response
     end
 
